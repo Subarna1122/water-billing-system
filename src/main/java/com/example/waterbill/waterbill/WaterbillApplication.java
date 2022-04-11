a59e2a4657863607359382d1a687284b974959d0
Subarna1122 <np03cs4s210181@heraldcollege.edu.np>
2022-04-11 19:59:01 +0545

    added files

---
+++ src/main/java/com/example/waterbill/waterbill/WaterbillApplication.java
@@ -0,0 +1,13 @@
+package com.example.waterbill.waterbill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterbillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterbillApplication.class, args);
+	}
+
+}
