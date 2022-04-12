65344b6e1b925d6bfce06f138b1368e23bc91366
SahilDai <np03cs4s210246@heraldcollege.edu.np>
2022-04-12 11:21:01 +0545

    added new line

--- src/main/java/com/example/waterbill/waterbill/WaterbillApplication.java
+++ src/main/java/com/example/waterbill/waterbill/WaterbillApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WaterbillApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting System");
 		SpringApplication.run(WaterbillApplication.class, args);
 	}
 
